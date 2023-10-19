1d768be7021b6efd690c081783e0025d5148a6e1
Jado Milton <jado.milton@gmail.com>
2023-10-19 11:21:33 +0200

    starRating

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React, { useState } from "react";
+import ReactDOM from "react-dom/client";
+// import App from './App'
+// import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import StarRating from "./StarRating";
+
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+  return (
+    <div>
+      <StarRating color="brown" onSetRating={setMovieRating} />
+      <p>this movie was rated {movieRating} stars</p>
+    </div>
+  );
+}
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    {/* <App /> */}
+    <StarRating color="green"
+      messages={["tres mal", "mal", "bon", "beaucoup bon", "tres bon"]}
+      maxRating={6}
+    />
+    <StarRating maxRating={10} defaultRating={3} />
+    <Test />
+  </React.StrictMode>
+);
